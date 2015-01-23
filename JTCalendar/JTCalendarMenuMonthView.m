341ef117b0cad0e285a2deb24abd21d1d9d2b859
Wenchao Ding <f33chobits@gmail.com>
2015-01-23 17:10:08 +0800

    Add alpha animation for menu month view
    Add alpha animation for menu month view

--- JTCalendar/JTCalendarMenuMonthView.m
+++ JTCalendar/JTCalendarMenuMonthView.m
@@ -10,6 +10,9 @@
 @interface JTCalendarMenuMonthView(){
     UILabel *textLabel;
 }
+@property (assign, nonatomic) BOOL autoDissolve;
+@property (assign, nonatomic) CGFloat autoDissolveMinAlpha;
+@property (assign, nonatomic) CGFloat autoDissolveMaxAlpha;
 
 @end
 
@@ -50,9 +53,31 @@
     }
 }
 
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"contentOffset"]) {
+        CGFloat offsetX = [self convertPoint:CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2) toView:self.window].x;
+        CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+        CGFloat alpha = _autoDissolveMaxAlpha - (ABS(screenWidth/2-offsetX)/(screenWidth/2))*(_autoDissolveMaxAlpha-_autoDissolveMinAlpha);
+        self.alpha = alpha;
+    }
+}
+
 - (void)setCurrentDate:(NSDate *)currentDate
 {
     textLabel.text = self.calendarManager.calendarAppearance.monthBlock(currentDate, self.calendarManager);
+}
+
+- (void)setAutoDissolve:(BOOL)autoDissolve
+{
+    if (_autoDissolve != autoDissolve) {
+        _autoDissolve = autoDissolve;
+        if (_autoDissolve) {
+            [self.calendarManager.menuMonthsView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
+        } else {
+            [self.calendarManager.menuMonthsView removeObserver:self forKeyPath:@"contentOffset"];
+        }
+    }
 }
 
 - (void)layoutSubviews
@@ -66,6 +91,14 @@
 {
     textLabel.textColor = self.calendarManager.calendarAppearance.menuMonthTextColor;
     textLabel.font = self.calendarManager.calendarAppearance.menuMonthTextFont;
+    _autoDissolveMinAlpha = self.calendarManager.calendarAppearance.autoDissolveMinAlpha;
+    _autoDissolveMaxAlpha = self.calendarManager.calendarAppearance.autoDissolveMaxAlpha;
+    self.autoDissolve = self.calendarManager.calendarAppearance.autoDissolveMenu;
+}
+
+- (void)dealloc
+{
+    [self.calendarManager.menuMonthsView removeObserver:self forKeyPath:@"contentOffset"];
 }
 
 @end
