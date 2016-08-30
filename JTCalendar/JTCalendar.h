26cf45fe044cbc6167ec1e3e3a266afe72a4a34e
Tom Izaks <tom@izaks.me>
2016-08-30 14:33:14 +1000

    Add support for Carthage

--- JTCalendar/JTCalendar.h
+++ JTCalendar/JTCalendar.h
@@ -5,14 +5,10 @@
 //  Created by Jonathan Tribouharet
 //
 
-#import "JTCalendarManager.h"
+#import <UIKit/UIKit.h>
 
-#import "JTHorizontalCalendarView.h"
-#import "JTVerticalCalendarView.h"
+//! Project version number for JTCalendar.
+FOUNDATION_EXPORT double JTCalendarVersionNumber;
 
-#import "JTCalendarMenuView.h"
-
-#import "JTCalendarPageView.h"
-#import "JTCalendarWeekDayView.h"
-#import "JTCalendarWeekView.h"
-#import "JTCalendarDayView.h"
+//! Project version string for JTCalendar.
+FOUNDATION_EXPORT const unsigned char JTCalendarVersionString[];
